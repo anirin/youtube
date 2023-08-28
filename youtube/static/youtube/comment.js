c536a8704e48681220b4028685715b531e2853a8
Ubuntu <ubuntu@ip-10-0-0-106.ap-northeast-1.compute.internal>
2023-08-28 01:14:32 +0000

    ajax done

--- youtube/static/youtube/comment.js
+++ youtube/static/youtube/comment.js
@@ -1,21 +1,17 @@
 window.addEventListener("load" , function (){
-    $("#submit").on("click", function(){ 
-        submit(); 
+    console.log("load");
+    $("#submit").on("click", function(e){ 
+		console.log("submit");
+        submit(e); 
     });
 });
 
 function submit(e){
     e.preventDefault();
-
     let form_elem   = "#form_area";
     let data    = new FormData( $(form_elem).get(0) );
     let url     = $(form_elem).prop("action");
     let method  = $(form_elem).prop("method");
-
-    //送信するデータの確認
-    console.log("hello")
-    for (let v of data ){ console.log(v); }
-    for (let v of data.entries() ){ console.log(v); }
 
     $.ajax({
         url: url,
@@ -30,13 +26,9 @@ function submit(e){
             console.log("ERROR");
         }
         else{
-            // console.log("Received content: ", data.content);
-            // print("-----\n")
-            // print(data.content)
-            // print("-----\n")
-            // $("#content_area").empty();
-            // $("#content_area").html(data.content);
-            // $("#textarea").val("");
+            $("#content_area").empty();
+            $("#content_area").html(data.content);
+            $("#form_area").val("");
         }
 
     }).fail( function(xhr, status, error) {
